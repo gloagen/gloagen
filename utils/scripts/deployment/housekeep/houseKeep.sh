3b715adfea8a54b4f798907e52cb52c32cf69020
mathieur <raphael.mathieu@gloagen.com>
2017-02-28 16:47:35 +0000

    DAK-16 #comment update scripts variable
    Signed-off-by: mathieur <raphael.mathieu@gloagen.com>

--- utils/scripts/deployment/housekeep/houseKeep.sh
+++ utils/scripts/deployment/housekeep/houseKeep.sh
@@ -3,10 +3,10 @@
 backup_last_release_spec() {
   echo "> backing up previous release.yml spec.."
   current_spec_file = "../properties/release.yml"
-  if [ -e current_spec_file ]; then
-    current_time = $(date + %d%m%y-%H%M%S.%N)
+  if [ -e "$current_spec_file" ]; then
+    current_time = $(date + %d%m%y%H%M%S%N)
     backup_spec_file = "../properties/release.backup.$current_time.yml"
-    mv $current_spec_file $backup_spec_file
+    mv "$current_spec_file" "$backup_spec_file"
 
   else
     echo "$current_spec_file has not been found.. nothing to backup!"
@@ -15,7 +15,7 @@ backup_last_release_spec() {
 
 for arg in "$@"
   do
-    if [ -n arg ] && [ arg = "--clean-release-spec" ]; then
+    if [ -n "$arg" ] && [ "$arg" = "--clean-release-spec" ]; then
       backup_last_release_spec
     fi
   done
