8b243c297fe6524e48fc63c5b415af727d8d4c22
mathieur <raphael.mathieu@gloagen.com>
2017-02-28 17:19:55 +0000

    DAK-16 #comment updated spec file variable definition
    Signed-off-by: mathieur <raphael.mathieu@gloagen.com>

--- utils/scripts/deployment/housekeep/houseKeep.sh
+++ utils/scripts/deployment/housekeep/houseKeep.sh
@@ -2,14 +2,18 @@
 
 backup_last_release_spec() {
   echo "> backing up previous release.yml spec.."
-  current_spec_file = "../properties/release.yml"
+
+  current_spec_file="../properties/release.yml"
+
   if [ -e "$current_spec_file" ]; then
+
     current_time = $(date + %d%m%y%H%M%S%N)
     backup_spec_file = "../properties/release.backup.$current_time.yml"
+    echo "> created backup file: $backup_spec_file"
     mv "$current_spec_file" "$backup_spec_file"
 
   else
-    echo "$current_spec_file has not been found.. nothing to backup!"
+    echo "> $current_spec_file has not been found.. nothing to backup!"
   fi
 }
 
