2a29e42430bb832a67443549d1c60cf8022dc742
mathieur <raphael.mathieu@gloagen.com>
2017-03-04 18:06:47 +0000

    DAK16 #comment updated install directory for a bundled release.
    Signed-off-by: mathieur <raphael.mathieu@gloagen.com>

---
+++ deployment/scripts/properties-env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DEPLOY_HOME=/opt/codedeploy-agent/deployment-root/gloagen-app
+export PATH=$PATH:$DEPLOY_HOME:$DEPLOY_HOME
--- utils/scripts/deployment/install/deployrelease-tomcat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from deployrelease import DeployRelease
-
-print("> Initialising deploymment release ...")
-deploy_r = DeployRelease()
-print("> Deployment release has been initialised, now deploying to tomcat...")
-deploy_r.deploy_to_tomcat()
-print("> Deployment to tomcat has been successfully completed")
