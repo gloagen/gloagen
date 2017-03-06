3b0ea1b46149d526ab5ade2974bde52a19ca97ef
mathieur <raphael.mathieu@gloagen.com>
2017-03-06 17:24:06 +0000

    DAK16 #comment update init env execution flow
    Signed-off-by: mathieur <raphael.mathieu@gloagen.com>

--- deployment/scripts/init-app-data.sh
+++ deployment/scripts/init-app-data.sh
@@ -1,51 +1,44 @@
 #!/bin/bash
-#current_time=$(date +"%d%m%yT%H%M%S.%N")
+current_time=$(date +"%d%m%yT%H%M%S.%N")
 
 log(){
-   echo "$1" >> "$GLOAG_DEPLOY_LOGS_HOME/deploy.log"
+   echo "$1" >> "$GLOAG_DEPLOY_LOGS_HOME/deploy-$current_time.log"
 }
 
-validate_environment_variables(){
-   if [ -n "$GLOAG_DEPLOY_HOME" ]; then
-      exit 104 # environment variable is not specified
-   fi
-
-   if [ ! -d "$GLOAG_DEPLOY_PROPERTIES_HOME/backup" ]; then
-      mkdir -p "$GLOAG_DEPLOY_PROPERTIES_HOME/backup"
-   fi
-
-   if [ ! -d "$GLOAG_DEPLOY_RELEASE_PROPERTIES_HOME" ]; then
-      mkdir -p "$GLOAG_DEPLOY_RELEASE_PROPERTIES_HOME"
-   fi
-
-   if [ ! -d "$GLOAG_DEPLOY_LOGS_HOME" ]; then
-      mkdir -p "$GLOAG_DEPLOY_LOGS_HOME"
-   fi
-}
-
-clean_all_properties_dir(){
-    rm -rf "$GLOAG_DEPLOY_RELEASE_PROPERTIES_HOME/*.*"
-    rm -rf "$GLOAG_DEPLOY_PROPERTIES_HOME/*.*"
+init_directories(){
+    if [ -n "$1" ]; then
+        exit 104 # environment variable is not specified
+    elif [ ! -d "$1" ]; then
+        mkdir -p "$1"
+    else
+        rm -rf "$1/*.*"
+    fi
 }
 
 copy_properties_to_app_dir(){
+    log "> copying files from ../properties/ to $GLOAG_DEPLOY_HOME/properties/"
     cp -TR ../properties/ "$GLOAG_DEPLOY_HOME/properties/"
+
+    log "> copying files from ../release/ to $GLOAG_DEPLOY_HOME/release/"
     cp -TR ../release/ "$GLOAG_DEPLOY_HOME/release/"
 }
 
-set_permissions(){
+prepare_deploy_dir() {
+    if [ -n "$GLOAG_DEPLOY_HOME" ] && [ -n "$GLOAG_DEPLOY_LOGS_HOME" ]; then
+        exit 104 # environment variable is not specified
+    fi
+
+    work_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+    cd "$work_dir"
+    log "> current working directory: $work_dir"
+
+    init_directories "$GLOAG_DEPLOY_RELEASE_PROPERTIES_HOME"
+    init_directories "$GLOAG_DEPLOY_PROPERTIES_HOME"
+
+    copy_properties_to_app_dir
+
     sudo chmod -R ug+rw "$GLOAG_DEPLOY_HOME/*.*"
     sudo chown gloag -R "$GLOAG_DEPLOY_HOME"
-}
-
-prepare_deploy_dir() {
-   work_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-   cd "$work_dir"
-   log "> current working directory: $work_dir"
-   validate_environment_variables
-   clean_all_properties_dir
-   copy_properties_to_app_dir
-   set_permissions
 }
 
 for arg in "$@"
