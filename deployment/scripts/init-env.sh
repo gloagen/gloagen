3435d62b0095be7bbeae880d0cc16de178c4697a
mathieur <raphael.mathieu@gloagen.com>
2017-03-06 01:16:00 +0000

    DAK16 #comment removed housekeep directory from execution
    Signed-off-by: mathieur <raphael.mathieu@gloagen.com>

--- deployment/scripts/init-env.sh
+++ deployment/scripts/init-env.sh
@@ -2,9 +2,9 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 sudo chmod ug+x deploy-properties-env.sh
-sudo chmod ug+x housekeep/*.sh
+sudo chmod ug+x *.sh
 sudo chmod ug+x install/*.py
 sudo chown gloag -R install/
 cp deploy-properties-env.sh /etc/profile.d/ # set the environment variables
 sh deploy-properties-env.sh                 # force load environment variables
-init-app-data.sh --prepare-directories # backup previous release spec
+./init-app-data.sh --prepare-directories # backup previous release spec
