0f930e08acf781d87639d8df6df14bf9d34436aa
mathieur <raphael.mathieu@gloagen.com>
2017-03-07 21:50:40 +0000

    DAK16 #comment exporting properties only
    Signed-off-by: mathieur <raphael.mathieu@gloagen.com>

--- deployment/scripts/install/deployrelease-tomcat.py
+++ deployment/scripts/install/deployrelease-tomcat.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-from scripts.install.deployrelease import DeployRelease
+from deployrelease import DeployRelease
 
-d_rel = DeployRelease()
-d_rel.init_logger()
-d_rel.deploy_to_tomcat()
+
+def deploy_tomcat(self):
+    d_rel = DeployRelease()
+    d_rel.init_logger()
+    d_rel.deploy_to_tomcat()
+
+
+deploy_tomact()
