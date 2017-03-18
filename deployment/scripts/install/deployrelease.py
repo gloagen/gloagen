e6e22461c00cae39a6f5f7fee557633a8f6c7706
MathieuR <raphael.mathieu@gloagen.com>
2017-03-18 10:04:33 +0000

    DAK-16 #COMMENT properties files are now resolved using relative path

--- deployment/scripts/install/deployrelease.py
+++ deployment/scripts/install/deployrelease.py
@@ -1,31 +1,18 @@
 import logging
 import os
-
+import pwd
+import grp
 import requests
 import yaml
 
-
-def call_clsinit(*args, **kwargs):
-    cls = type(*args, **kwargs)
-    cls._clsinit()
-    return cls;
 
 class DeployRelease:
     """ Utility class to read the user service relased archive
         and update tomcat with the downloaded release """
 
-    __metaclass__ = call_clsinit
-
-    @classmethod
-    def _clsinit(cls):
-        init_logger()
-
     def init_logger(self):
-        log_file = os.path.join(os.environ.get('GLOAG_DEPLOY_HOME'), "logs", "deployrelease.log")
-        # logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(message)s')
+        log_file = "../logs/deployrelease.log"
         lHandler = logging.FileHandler(log_file, mode='a', encoding=None, delay=False)
-        # lHandler = logging.RotatingFileHandler(log_file, mode='a', maxBytes=1000000, backupCount=5, encoding=None,
-        #                                             delay=0)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         lHandler.setFormatter(formatter)
         logger = logging.getLogger('deploy-release')
@@ -36,47 +23,37 @@ class DeployRelease:
     def ___init___(self):
         self.data = []
 
-    def get_release_properties(self):
-        release_file_path = os.path.join(os.environ.get('GLOAG_DEPLOY_RELEASE_PROPERTIES_HOME'), "userservice.yml")
-        return self.read_data(release_file_path)
-
-    def get_app_properties(self):
-        appProperties_file_path = os.path.join(os.environ.get('GLOAG_DEPLOY_PROPERTIES_HOME'), "app-properties.yml")
-        return self.read_data(appProperties_file_path)
-
     def read_data(self, filename):
         self.logger.info("attempting to read data with file name: " + filename)
         with open(filename, "r") as filedata:
             return yaml.load(filedata)
 
     def generate_artifact_download_url(self):
-        releaseProps = self.get_release_properties()
-        url = self.get_app_properties()['artifactory']['repository']['release']['url']
-        url = url.replace("{groupid-directory}", releaseProps['release']['groupid-directory'])
-        url = url.replace("{app-name}", releaseProps['release']['name'])
-        url = url.replace("{version}", releaseProps['release']['version'])
-        url = url.replace("{type}", releaseProps['release']['type'])
+        url = self.appProperties['artifactory']['repository']['release']['url'] \
+            .replace("{groupid-directory}", self.releaseProperties['release']['groupid-directory']) \
+            .replace("{app-name}", self.releaseProperties['release']['name']) \
+            .replace("{version}", self.releaseProperties['release']['version']) \
+            .replace("{type}", self.releaseProperties['release']['type'])
         self.logger.info("generated artifact url is: " + url)
         return url
 
     def build_release_filename(self):
-        releaseProps = self.get_release_properties()
-        return releaseProps['tomcat']['target-name'] + "." + releaseProps['release']['type']
+        return self.releaseProperties['tomcat']['target-name'] + "." + self.releaseProperties['release']['type']
 
     def create_artifact_download_fullpath(self):
         appProperties = self.get_app_properties()
-        filename = os.path.join(os.environ.get('GLOAG_DEPLOY_DOWNLOADS_HOME'), self.build_release_filename())
+        filename = os.path.join("../downloads", self.build_release_filename())
         self.logger.info("generated artifact full path is : ", filename)
         return filename
 
     def get_artifact_repository_login_user(self):
-        return self.get_app_properties()['artifactory']['user']
+        return self.appProperties['artifactory']['user']
 
     def get_artifact_repository_login_password(self):
-        return self.get_app_properties()['artifactory']['password']
+        return self.appProperties['artifactory']['password']
 
     def get_artifact_download_chunk_size(self):
-        return self.get_app_properties()['archive']['download']['chunk_size']
+        return self.appProperties['archive']['download']['chunk_size']
 
     def download_released_artifact(self):
         download_url = self.generate_artifact_download_url()
@@ -94,7 +71,7 @@ class DeployRelease:
             for chunk in req.iter_content(chunk_size=self.get_artifact_download_chunk_size()):
                 relFile.write(chunk)
 
-        self.logger.info("Download has now completed")
+        self.logger.info("Download has now completed for artifact: "+ artifactPath)
         return artifactPath
 
     def get_tomcat_webapps_directory(self):
@@ -109,22 +86,33 @@ class DeployRelease:
             os.remove(filename)
             self.logger.info("file have now been deleted.")
         else:
-            self.logger.info("file not founf in tomcat directory!")
+            self.logger.info("file not found in tomcat directory!")
         return filename
 
     def deploy_to_tomcat(self):
         try:
+
             self.load_properties()
             self.logger.info("commencing deplyment to tomcat..")
             artifact = self.download_released_artifact()
-            self.logger.info("downloaded articfact:", artifact)
+
+            uid = pwd.getpwnam("gloag").pw_uid
+            gid = grp.getgrnam("gloag").gr_gid
+            os.chown(artifact, uid, gid)
+            self.logger.info("change artifact ownership to gloag")
+
             targetArtifact = self.remove_previous_deployment()
             os.rename(artifact, targetArtifact)
             self.logger.info("deployment completed.")
+
         except Exception as err:
             self.logger.error("Caught an exception whilst attempting to deploy to tomcat: {0}".format(err), err)
             raise
 
     def load_properties(self):
+        self.init_logger()
         path = "../properties/app-properties.yml"
         self.appProperties = self.read_data(path)
+        self.logger.info("loaded application properties")
+        self.releaseProperties = self.read_data("../properties/release.yml")
+        self.logger.info("loaded release properties")
