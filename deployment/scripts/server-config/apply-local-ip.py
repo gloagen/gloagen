a350102747259baba2f0c8ebbd02070cbdf3ec7f
MathieuR <raphael.mathieu@gloagen.com>
2017-03-19 07:37:28 +0000

    added debug info

--- deployment/scripts/server-config/apply-local-ip.py
+++ deployment/scripts/server-config/apply-local-ip.py
@@ -5,6 +5,7 @@ from deployconfig import DeployConfig
 def apply_local_ip():
     try:
         config = DeployConfig()
+        config.init_logger()
         config.find_replace_token_in_file(filePath="../../configs/server/wildfly/standalone-full.xml", \
                                   token='{internal-ip}', \
                                   replaceToken=config.get_local_ip())
--- deployment/scripts/server-config/deployconfig.py
+++ deployment/scripts/server-config/deployconfig.py
@@ -8,14 +8,27 @@ import fileinput
 
 class DeployConfig:
 
+    def init_logger(self):
+        log_file = "../../logs/deployconfig.log"
+        lHandler = logging.FileHandler(log_file, mode='a', encoding=None, delay=False)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        lHandler.setFormatter(formatter)
+        logger = logging.getLogger('deploy-config')
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(lHandler)
+        self.logger = logger
+
     def get_local_ip(self):
         r = requests.get('http://instance-data/latest/meta-data/local-ipv4')
         self.local_ip = r.text
+        self.logger.info("retrieved local server ip: "+self.local_ip)
         return self.local_ip
 
     def find_replace_token_in_file(self, filePath, token, replaceToken):
+        self.logger.info("attempting to relace token: "+token+ " with: "+replaceToken+ " in file: "+filePath)
         with fileinput.FileInput(filePath, inplace=True, backup='.bak') as file:
             for line in file:
+                self.logger.debug("read line: "+line)
                 print (line.replace(token, replaceToken))
 
 
